2b86ce3b936c27b49f7d3e235a3928e9e4b487c6
“Tsubasa <e235730@ie.u-ryukyu.ac.jp>
2024-01-16 00:52:57 +0900

    カード作成

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e235730/Card.java
@@ -0,0 +1,20 @@
+package jp.ac.uryukyu.ie.e235730;
+
+public class Card {
+    private String color;
+    private String number;
+
+    public Card(String color, String number){
+        this.color=color;
+        this.number=number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getValue() {
+        return number;
+    }
+
+}
