4dceeba4f8dd5c19bfa6a9009426d0fdbe3c133e
“Tsubasa <e235730@ie.u-ryukyu.ac.jp>
2024-01-18 13:19:00 +0900

    CPU追加

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e235730/CPUPlayer.java
@@ -0,0 +1,9 @@
+package jp.ac.uryukyu.ie.e235730;
+
+import java.util.List;
+
+public class CPUPlayer extends Player {
+    public CPUPlayer(String name,List<Card>hand){
+        super(name,hand);
+    }
+}
--- app/src/main/java/jp/ac/uryukyu/ie/e235730/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235730/Main.java
@@ -1,19 +1,39 @@
 package jp.ac.uryukyu.ie.e235730;
+
 import java.util.ArrayList;
 import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Deck deck = new Deck();
         deck.shuffle();
         
-        List<Card> hand = new ArrayList<>();
+        List<Card> hand1 = new ArrayList<>();
         for (int i = 0; i < 7; i++) {
-            hand.add(deck.drawCard());
+            hand1.add(deck.drawCard());
+        }
+        List<Card> hand2 = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            hand2.add(deck.drawCard());
+        }
+        List<Card> hand3 = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            hand3.add(deck.drawCard());
+        }
+        List<Card> hand4 = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            hand4.add(deck.drawCard());
         }
         
-        Player player = new Player("Player1", hand);
+        Player player = new Player("Player1", hand1);
+        Player CPU2 = new CPUPlayer("CPU1", hand2);
+        Player CPU3 = new CPUPlayer("CPU1", hand3);
+        Player CPU4 = new CPUPlayer("CPU1", hand4);
         
         player.displayHand();
-        
+        CPU2.displayHand();
+        CPU3.displayHand();
+        CPU4.displayHand();
+
     }
 }
