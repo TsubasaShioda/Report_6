7789d9ae144f61f6649c3cbb25f173619767f95c
“Tsubasa <e235730@ie.u-ryukyu.ac.jp>
2024-01-21 15:36:36 +0900

    CPU対戦追加

--- app/src/main/java/jp/ac/uryukyu/ie/e235730/Deck.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235730/Deck.java
@@ -9,7 +9,7 @@ public class Deck {
     
     public Deck() {
         cards = new ArrayList<>();
-        for (int i=0; i<2; i++)
+        for (int i=0; i<7; i++)
             for (String color : new String[]{"red", "blue", "yellow", "green"}) {
                 for (String number : new String[]{"0","1","3","4","5","6","7","8","9"}) {
                     cards.add(new Card(color, number));
--- app/src/main/java/jp/ac/uryukyu/ie/e235730/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235730/Main.java
@@ -3,6 +3,7 @@ package jp.ac.uryukyu.ie.e235730;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -31,42 +32,72 @@ public class Main {
         Player CPU2 = new CPUPlayer("CPU2", hand2);
         Player CPU3 = new CPUPlayer("CPU3", hand3);
         
+        List<Player> players = new ArrayList<>();
+        players.add(player);
+        players.add(CPU1);
+        players.add(CPU2);
+        players.add(CPU3);
+        
         player.displayHand();
-        CPU1.displayHand();
-        CPU2.displayHand();
-        CPU3.displayHand();
+
         Scanner scanner = new Scanner(System.in);
 
         Card currentCard = deck.drawCard();
+        int currentPlayerIndex = 0;
 
         while (true) {
-            player.displayHand();
+            Player currentPlayer = players.get(currentPlayerIndex);
+            
+            System.out.println("現在のプレイヤー" + currentPlayer.getName());
             System.out.println("現在のカード：色：" + currentCard.getColor() + "  数字：" + currentCard.getNumber());
 
             boolean validPlay = false;
-            while (!validPlay) {
-                System.out.println("プレイするカードの番号を入力してください（0から" + (player.getNumCardsInHand() - 1) + "）:");
-                int cardIndex = scanner.nextInt();
-
-                Card selectedCard = player.moveCard(cardIndex);
+            
+            if (currentPlayer instanceof CPUPlayer) {
+                int cardIndex = new Random().nextInt(currentPlayer.getNumCardsInHand());
+                Card selectedCard = currentPlayer.moveCard(cardIndex);
 
                 if (selectedCard.getColor().equals(currentCard.getColor())
                         || selectedCard.getNumber().equals(currentCard.getNumber())) {
                     validPlay = true;
                     currentCard = selectedCard;
-                    player.playCard(selectedCard);
+                    currentPlayer.playCard(selectedCard);
+                    System.out.println(currentPlayer.getName() + "は 色：" + currentCard.getColor() + "  数字：" + currentCard.getNumber() + "を出した！");
                 } else {
-                    System.out.println("選択したカードはプレイできません。別のカードを選択してください。");
-                    player.drawCard(selectedCard);
+                    currentPlayer.drawCard(selectedCard);
+                    System.out.println(currentPlayer.getName() + "はパスした");
+                    currentPlayer.getHand().add(deck.drawCard());
+                }
+            } else {
+                while (!validPlay) {
+                    player.displayHand();
+                    System.out.println("現在のカード：色：" + currentCard.getColor() + "  数字：" + currentCard.getNumber());
+                    System.out.println("プレイするカードの番号を入力してください（0から" + (currentPlayer.getNumCardsInHand() - 1) + "）:");
+                    int cardIndex = scanner.nextInt();
+
+                    Card selectedCard = currentPlayer.moveCard(cardIndex);
+
+                    if (selectedCard.getColor().equals(currentCard.getColor())
+                            || selectedCard.getNumber().equals(currentCard.getNumber())) {
+                        validPlay = true;
+                        currentCard = selectedCard;
+                        currentPlayer.playCard(selectedCard);
+                    } else {
+                        System.out.println("選択したカードはプレイできません。別のカードを選択してください。");
+                        currentPlayer.drawCard(selectedCard);
+                        hand.add(deck.drawCard());
+                        validPlay = true;
+                    }
                 }
             }
             
-            if (player.getHand().isEmpty()) {
-                System.out.println("プレイヤーの勝ち！");
+            if (currentPlayer.getHand().isEmpty()) {
+                System.out.println(currentPlayer.getName() + "の勝ち！");
                 break;
             }
+            
+            currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
         }
         scanner.close();
     }
-
 }
